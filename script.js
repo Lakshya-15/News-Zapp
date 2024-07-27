ea23ea87ab824b7e68db1181874dae2512df7273
Lakshya Garg <garglakshya015@gmail.com>
2024-07-28 00:56:42 +0530

    Update script.js
    updated the API key

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "5b30fa04ae8c44f088b55d43f9f69922";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
